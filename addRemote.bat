d2986cd0fa34d9fbf71eb536a257d8a8fbbdb450
qiuhong_cn <1195109313@qq.com>
2024-11-05 01:47:30 +0000

    add addRemote.bat.
    Signed-off-by: qiuhong_cn <1195109313@qq.com>

---
+++ addRemote.bat
@@ -0,0 +1,2 @@
+git remote set-url --add origin git@atomgit.com:qiuyuki/h5_render_engine_performance_test.git
+git remote set-url --add origin git@github.com:cn-qiuhong/h5_render_engine_performance_test.git
