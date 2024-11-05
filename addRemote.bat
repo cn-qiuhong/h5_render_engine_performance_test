2756f1ece9cb685fc04edf180654bf30569ae5b5
qiuhong <qiu.hong@foundbyte.com>
2024-11-05 09:55:57 +0800

    fix

--- addRemote.bat
+++ addRemote.bat
@@ -1,3 +1,2 @@
-git remote set-url --add origin git@gitee.com:qiuhong_cn/h5_render_engine_performance_test.git
 git remote set-url --add origin git@atomgit.com:qiuyuki/h5_render_engine_performance_test.git
 git remote set-url --add origin git@github.com:cn-qiuhong/h5_render_engine_performance_test.git
